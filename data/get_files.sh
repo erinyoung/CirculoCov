6dc4fc44921c776bfac3449a37f7b448aeab12e5
Erin Young <erin.olde@gmail.com>
2023-09-13 20:29:51 +0000

    added bedtools and path

--- data/get_files.sh
+++ data/get_files.sh
@@ -1,7 +1,6 @@
 # get nanopore reads
 wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR233/016/SRR23387316/SRR23387316_1.fastq.gz
 
-
 # get illumina reads
 wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR230/067/SRR23080367/SRR23080367_1.fastq.gz
 wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR230/067/SRR23080367/SRR23080367_2.fastq.gz
@@ -14,3 +13,9 @@ wget https://www.ebi.ac.uk/ena/browser/api/fasta/GCA_021664795.2?download=true -
 
 # get minimap2
 curl -L https://github.com/lh3/minimap2/releases/download/v2.26/minimap2-2.26_x64-linux.tar.bz2 | tar -jxvf -
+
+wget https://github.com/arq5x/bedtools2/releases/download/v2.31.0/bedtools.static -O bedtools
+
+chmod +x bedtools
+
+export PATH=$(pwd)/minimap2-2.26_x64-linux:$(pwd):$PATH
