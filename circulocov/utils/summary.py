503ddc8006f5d835d4ad40cf1272d08f514aa010
Erin Young <erin.olde@gmail.com>
2024-01-19 15:11:52 -0700

    added length

--- circulocov/utils/summary.py
+++ circulocov/utils/summary.py
@@ -15,8 +15,8 @@ def summary(df_cov, genome_dict, results_dict, args):
 
     df['circ'] = 'X'
     for contig in genome_dict.keys():
-        print(genome_dict[contig])
         df.loc[df['#rname'] == contig, 'circ'] = genome_dict[contig]['circ']
+        df.loc[df['#rname'] == contig, 'endpos'] = genome_dict[contig]['length']
 
     # now to get the dataframe pretty
     first_column = df.pop('circ')
@@ -25,7 +25,7 @@ def summary(df_cov, genome_dict, results_dict, args):
     first_column = df.pop('sample')
     df.insert(0, 'sample', first_column)
 
-    df = df.sort_values(by=['endpos', '#rname'], ascending= [False, True], ignore_index=True)
+    df = df.sort_values(by=['endpos', '#rname'], ascending= [False, True], ignore_index=True)    
     df = df.rename(columns={'#rname': 'contigs', 'endpos': 'length'})
 
     df.to_csv(args.out + '/overall_summary.txt', index=False, sep = "\t")
