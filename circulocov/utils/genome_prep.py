d9e3c562a859cb76c2057437219573a111acbdb5
Erin Young <erin.olde@gmail.com>
2024-01-31 11:24:48 -0700

    adding GC

--- circulocov/utils/genome_prep.py
+++ circulocov/utils/genome_prep.py
@@ -6,6 +6,7 @@
 import logging
 import os
 from Bio import SeqIO
+from Bio.SeqUtils import GC
 
 def genome_prep(args, out):
     """ Turn fasta file into dict """
@@ -22,6 +23,7 @@ def genome_prep(args, out):
     for record in SeqIO.parse(args.genome,"fasta"):
         genome_dict[record.id] = {}
         genome_dict[record.id]["length"] = len(record.seq)
+        genome_dict[record.id]["GC"] = GC(record.seq)
         total_length = total_length + len(record.seq)
 
         # checking if circular
