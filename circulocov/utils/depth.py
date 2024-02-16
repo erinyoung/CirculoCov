b8fa071dbfdcd026a66858f6bda0e809ef8f45af
Erin Young <erin.olde@gmail.com>
2024-02-15 17:25:37 -0700

    no longer doing ave

--- circulocov/utils/depth.py
+++ circulocov/utils/depth.py
@@ -1,33 +1,14 @@
 #!/usr/bin/env python
-# pylint: disable=logging-fstring-interpolation
 
-""" Gets depth for bam """
+''' Gets depth for bam '''
 
-import logging
 import pysam
-import pandas as pd
 
-def depth(bam, reg):
-    """ Gets coverage for bam over subrange """
+def depth(bam, contig, tmp):
+    ''' Gets coverage for bam for contig '''
 
-    depths = pysam.depth(bam, "-r", reg).split("\n")
+    depth_file = tmp + '/' + contig + '.txt'
 
-    df = pd.DataFrame(columns = ["ref", "pos", "depth"])
-    for dep in depths:
-        dept = dep.split("\t")
-        if len(dept) == 3:
-            df.loc[len(df.index)] = dept
+    pysam.depth(bam, '-r', contig, '-o', depth_file )
 
-    logging.debug(f"The depth dataframe for {bam}")
-    logging.debug(df)
-
-    if df.empty:
-        ave = 0.0
-    else:
-        df["depth"] = pd.to_numeric(df["depth"])
-
-        ave = df["depth"].mean()
-
-    logging.debug(f"The average for {reg} is {ave}")
-
-    return reg, ave
+    return depth_file
